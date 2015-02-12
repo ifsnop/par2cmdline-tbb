efd62afae9b9ad60fa35096db52ffa282fc05013
Diego Torres <diego.torres@gmail.com>
2015-02-12 17:54:37 +0100

    Reduce number of loops by UpdateVerificationResults
    Only run UpdateVerificationResults at the end of a files subset check.
    Now it was run on every file checked which is not necessary.
    
    This could possibly speed up some things.
    
    BlackEagle <ike.devolder@gmail.com>
    https://github.com/BlackIkeEagle/par2cmdline/commit/ff73d29a31d2a98e666e0e833591f428846d0d16

--- par2repairer.cpp
+++ par2repairer.cpp
@@ -1771,9 +1771,6 @@ bool Par2Repairer::VerifySourceFiles(void)
 
       // We have finished with the file for now
       diskfile->Close();
-
-      // Find out how much data we have found
-      UpdateVerificationResults();
     }
     else
     {
@@ -1788,6 +1785,9 @@ bool Par2Repairer::VerifySourceFiles(void)
 
     ++sf;
   }
+
+  // Find out how much data we have found
+  UpdateVerificationResults();
 #endif
   return finalresult;
 }
@@ -1863,12 +1863,12 @@ bool Par2Repairer::VerifyExtraFiles(const list<CommandLine::ExtraFile> &extrafil
 
         // We have finished with the file for now
         diskfile->Close();
-
-        // Find out how much data we have found
-        UpdateVerificationResults();
       }
     }
   }
+
+  // Find out how much data we have found
+  UpdateVerificationResults();
 
   return true;
 #endif
@@ -3454,10 +3454,10 @@ bool Par2Repairer::VerifyTargetFiles(void)
 
     // Close the file again
     targetfile->Close();
-
-    // Find out how much data we have found
-    UpdateVerificationResults();
   }
+
+  // Find out how much data we have found
+  UpdateVerificationResults();
 
   return finalresult;
 }
